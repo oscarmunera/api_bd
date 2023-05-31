1488e9c06bbbab2698e35c9dd344934a648b46e4
unoycerodigital <oscar.munera@unoycerodigital.com.co>
2023-05-31 16:41:48 -0500

    creado archivo de conexión a bd

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
 # URL de conexión a la base de datos PostgreSQL en Supabase
-db_url = "postgresql://usuario:contraseña@host:puerto/nombre_basedatos"
+db_url = "postgresql://postgres:nmaC9kFba7@vm6W@db.raredzkxqyzlbgijrlmh.supabase.co:5432/postgres"
 
 # Crear el motor de SQLAlchemy para la conexión a la base de datos
 engine = create_engine(db_url)
