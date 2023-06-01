19c6f83f805b58eb46675e917c69493733b896ac
unoycerodigital <oscar.munera@unoycerodigital.com.co>
2023-06-01 18:25:22 -0500

    creación de modelosorm y esquemas pydantic

--- sql_app/database.py
+++ sql_app/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:3xFUluXkqVtYBNkP@db.raredzkxqyzlbgijrlmh.supabase.co:5432/postgres"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
---
+++ sql_app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column,Integer,String
+from sql_app.database import Base
+
+class libro(Base):
+    __tablename__ = "libros"
+
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String)
+    autor = Column(String)
+    anio_publicacion = Column(Integer)
+    genero = Column(String)
